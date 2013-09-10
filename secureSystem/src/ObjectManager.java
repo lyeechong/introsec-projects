b8727785aeb9d3f0f142df1f9909c44474ebe0c2
Lyee Chong <lchong@cs.utexas.edu>
2013-09-09 21:43:24 -0500

    I think its going well

--- secureSystem/src/ObjectManager.java
+++ secureSystem/src/ObjectManager.java
@@ -12,44 +12,38 @@ public class ObjectManager {
     public ObjectManager() {
     }
 
-    private InstructionObject performWrite(SystemObject so, SystemSubject ss, int valToWrite) {
+    private void performWrite(SystemObject so, SystemSubject ss, int valToWrite) {
         so.setValue(valToWrite);
-        return new InstructionObject(ss.getName() + " writes value " + valToWrite + " to " + so.getName());
-    }
-    
-    private InstructionObject performRead(SystemObject so, SystemSubject ss) {
-        int value = so.getValue();
-        ss.setTempValue(value);
-        return new InstructionObject(ss.getName() + " reads " + so.getName());
     }
 
-    public InstructionObject performInstruction(String instruction) {
+    private void performRead(SystemObject so, SystemSubject ss) {
+        int value = so.getValue();
+        ss.setTempValue(value);
+    }
+
+    public void performInstruction(String instruction) {
         if (TokenHelper.isRead(instruction)) {
             InstructionObject result;
-            
+
             String subjName = TokenHelper.getSubjectName(instruction);
             String objName = TokenHelper.getObjectName(instruction);
-            
+
             SystemSubject ss = SystemSubjectsContainer.get(subjName);
             SystemObject so = SystemObjectsContainer.get(objName);
-            
-            result = performRead(so, ss);
-            return result;
+            performRead(so, ss);
+
         } else if (TokenHelper.isWrite(instruction)) {
             InstructionObject result;
-            
+
             String subjName = TokenHelper.getSubjectName(instruction);
             String objName = TokenHelper.getObjectName(instruction);
-            int val = Integer.parseInt(TokenHelper.obtainTokenAtIndex(instruction, 3));                    
-            
+            int val = Integer.parseInt(TokenHelper.obtainTokenAtIndex(instruction, 3));
+
             SystemSubject ss = SystemSubjectsContainer.get(subjName);
             SystemObject so = SystemObjectsContainer.get(objName);
-            
-            result = performWrite(so, ss, val);
-            return result;
+            performWrite(so, ss, val);
         } else {
             assert false;
-            return null;
         }
     }
 }
--- secureSystem/src/ReferenceMonitor.java
+++ secureSystem/src/ReferenceMonitor.java
@@ -1,6 +1,4 @@
 
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Whenever a subject wishes to perform an action, it is directed here and
@@ -11,47 +9,54 @@ import java.util.Map;
  *
  * @author lchong
  */
-public class ReferenceMonitor
-{
+public class ReferenceMonitor {
 
     InstructionChecker instructionChecker;
-    TokenHelper objectManager;
+    ObjectManager objectManager;
     ClearanceChecker cc;
 
     /**
      * Constructor. Inits the subjectSecurityMap and the objectSecurityMap.
      */
-    public ReferenceMonitor()
-    {
+    public ReferenceMonitor() {
         instructionChecker = new InstructionChecker();
         objectManager = new TokenHelper();
         cc = new ClearanceChecker();
     }
-    
-    public InstructionObject performInstruction(String instruction)
-    {
+
+    public InstructionObject performInstruction(String instruction) {
+        
+        InstructionObject result = new InstructionObject("Something went terribly wrong.");
+        
         boolean validInstruction = instructionChecker.isInstructionIsLegal(instruction);
-        if(!validInstruction) //syntax checking only
+        if (!validInstruction) //syntax checking only
         {
             // -- the instruction was illegal, so generate a BadInstruction
             return new BadInstruction("Bad Instruction");
         }
-        
+
         //TODO: check the security clearance stuff here
         //read down
         //write up
-        
-        objectManager.obtainTokenAtIndex(instruction, index);
-        
-        SystemSubject so = SystemSubjectsContainer.get(subjName);
-        SystemObject so = SystemObjectsContainer.get(objName);
-        cc.hasClearance();
-        
-        // -- perform the instruction since it was valid (via the ObjectManager)
-        InstructionObject result = objectManager.performInstruction(instruction);
-        //TODO fill out the rest of this method
-        
-        
+
+
+        SystemSubject ss = SystemSubjectsContainer.get(TokenHelper.getSubjectName(instruction));
+        SystemObject so = SystemObjectsContainer.get(TokenHelper.getObjectName(instruction));
+        boolean hasClearance = cc.hasClearance(ss, so, instruction);
+
+        if (TokenHelper.isRead(instruction)) {
+            result = new InstructionObject(ss.getName() + " reads " + so.getName());
+        } else if (TokenHelper.isWrite(instruction)) {
+            result = new InstructionObject(ss.getName() + " writes value " + TokenHelper.obtainTokenAtIndex(instruction, 3) + " to " + so.getName());
+        } else {
+            assert false;
+        }
+
+        if (hasClearance) {
+            // -- perform the instruction since it was valid (via the ObjectManager)
+            objectManager.performInstruction(instruction);
+        }
+
         return result;
     }
 }
