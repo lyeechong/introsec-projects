2b451004e1132b16bd95dce15fa273651e769e69
Lyee Chong <lchong@cs.utexas.edu>
2013-09-24 16:55:17 -0500

    moving seats

---
+++ secureSystem/src/CovertChannel.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lchong
+ */
+public class CovertChannel
+{
+
+    public CovertChannel()
+    {
+    }
+
+    public static void main(String[] args)
+    {
+    }
+}
