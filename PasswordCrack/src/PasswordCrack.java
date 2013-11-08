8c8acb6872f67af34ff57c49637feaa54c474abc
Lyee Chong <lchong@cs.utexas.edu>
2013-11-08 15:02:02 -0600

    assign 5 started

---
+++ PasswordCrack/src/PasswordCrack.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lchong
+ */
+public class PasswordCrack
+{
+    public PasswordCrack()
+    {
+        
+    }
+    
+    public static void main(String[] args)
+    {
+        PasswordCrack pc = new PasswordCrack();
+        pc.run(args);
+    }
+    
+    public void run(String[] args)
+    {
+        
+    }
+}
